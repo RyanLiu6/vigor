be960c5eca6136beac882725e6f1cbf3b33ac785
Ryan Liu <ryan@ryanliu6.xyz>
2024-01-09 12:35:11 -0800

    Updating empty_directory_remover

--- vigor/scripts/files/empty_directory_remover.py
+++ vigor/scripts/files/empty_directory_remover.py
@@ -5,13 +5,14 @@ import shutil
 
 import click
 
-from pathlib import Path
 
+to_delete = []
 
 @click.command()
 @click.argument("root_dir")
 @click.option("--dry-run", is_flag=True, default=False, help="Dry Run")
-def remove_empty_directories(root_dir: str, dry_run: bool):
+@click.option("-r", "--recursive", is_flag=True, default=False, help="Should recursively check sub-directories")
+def remove_empty_directories(root_dir: str, dry_run: bool, recursive: bool):
     """
     Finds folders in root_dir that only contain an .nfo file, without a corresponding media file.
     In this case, these folders are "empty", and should be removed.
@@ -19,20 +20,24 @@ def remove_empty_directories(root_dir: str, dry_run: bool):
     Args:
         root_dir (str): Root Directory to scan. Only looks at first level subdirectories.
     """
-    to_delete = []
+    inner_function(root_dir=root_dir, recursive=recursive)
 
-    for path, _, files in os.walk(root_dir):
+    for item in to_delete:
+        file_path = os.path.join(item[1], item[0])
+        print(f"Deleting file {file_path} and directory {item[1]}")
+        if not dry_run:
+            os.remove(file_path)
+            os.rmdir(item[1])
+
+
+def inner_function(root_dir: str, recursive: bool):
+    for path, subdir, files in os.walk(root_dir):
         if len(files) == 1 and ".nfo" in files[0]:
             to_delete.append((files[0], path))
 
-
-    print(dry_run)
-
-    for item in to_delete:
-        print(f"Deleting file {item[0]} and directory {item[1]}")
-        if not dry_run:
-            os.remove(os.path.join(item[1], item[0]))
-            os.rmdir(item[1])
+        if recursive:
+            for sub in subdir:
+                inner_function(root_dir=sub, recursive=recursive)
 
 
 if __name__ == "__main__":
