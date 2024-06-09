4a2578b99ccee4e36a03a4d49daf9d198a06e15a
Ryan Liu <ryan@ryanliu6.xyz>
2024-06-09 01:07:02 -0700

    Fixing bug with get_immediate_subdirectories and Compose

--- vigor/utils.py
+++ vigor/utils.py
@@ -12,11 +12,11 @@ def get_immediate_subdirectories(full_path: str, ignore: List=[]) -> List[str]:
     Get all immediate sub-directories for a certain full path.
 
     Args:
-        full_path (str): Full path to folder to get immediate sub-directories for.
+        full_path (str): Path (absolute) to folder to get immediate sub-directories for.
         ignore (List): List of folders to ignore. Defaults to [].
 
     Returns:
-        List: List of immediate sub-directories.
+        List: List of immediate sub-directory names.
     """
     if not os.path.isabs(full_path):
         raise ValueError("Parameter full_path must be an Absolute Path!")
@@ -24,7 +24,7 @@ def get_immediate_subdirectories(full_path: str, ignore: List=[]) -> List[str]:
     subdirectories = []
     for item in os.scandir(full_path):
         if item.is_dir() and item.name not in ignore:
-                subdirectories.append(item.path)
+                subdirectories.append(item.name)
 
     return subdirectories
 
